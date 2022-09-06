efe8a8d468afc2e754b58140c8b50951b2f27017
Arvind <europa1613@users.noreply.github.com>
2022-09-06 13:55:29 -0500

    Dockerfile from scratch - go program

---
+++ dockerfile/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello Docker from scratch!!")
+}
+
+// Build with the following command from the same dir as this file
+// env GOARCH=386 GOOS=linux go build hello.go
